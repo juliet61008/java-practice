9fbe55496204a1cb52ab9030b7ed1b14cf4ba900
juliet61008 <juliet61008@gmail.com>
2026-01-29 17:55:32 +0900

    최단거리 dfs 적용

--- src/programmers/게임_맵_최단거리/Solution.java
+++ src/programmers/게임_맵_최단거리/Solution.java
@@ -8,18 +8,33 @@ public class Solution {
     public static void main(String[] args) {
         int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
 
-        int n = maps[0].length;
-        int m = maps.length;
-
         Solution solution = new Solution();
 
         solution.solution(maps);
     }
 
     public int solution(int[][] maps) {
+
+        // 행
+        int[] dy = {-1, 1, 0, 0};
+        // 열
+        int[] dx = {0, 0, -1, 1};
+
         int answer = 0;
+
+        // 위치
+        // 행
+        int n = 1;
+        // 열
+        int m = 1;
+
+        // 행 사이즈
+        int sizeN = maps.length;
+        // 열 사이즈
+        int sizeM = maps[0].length;
 
         return answer;
     }
+
 
 }
