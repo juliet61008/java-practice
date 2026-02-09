cf732fa9de8c5e8daa2e0ab87d5c4b43aecfa5fa
juliet61008 <juliet61008@gmail.com>
2026-02-09 10:24:36 +0900

    insertionSort

---
+++ src/algorithm/sort/insertion/InsertionSort2.java
@@ -0,0 +1,30 @@
+package algorithm.sort.insertion;
+
+import java.util.Arrays;
+
+public class InsertionSort2 {
+    public static void main(String[] args) {
+        int[] a = {5,4,3,2,1};
+
+        insertionSort(a);
+
+        Arrays.stream(a).forEach(System.out::print);
+
+    }
+
+    public static void insertionSort(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+
+            int target = a[i];
+
+            int j = i - 1;
+
+            while (j >= 0 && target < a[j]) {
+                a[j + 1] = a[j];
+                j--;
+            }
+
+            a[j + 1] = target;
+        }
+    }
+}
