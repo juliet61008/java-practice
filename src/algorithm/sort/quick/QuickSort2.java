b63f0bf2d5f4abb26ecc3d200b93940022b2430b
juliet61008 <juliet61008@gmail.com>
2026-02-10 09:54:50 +0900

    quickSort

---
+++ src/algorithm/sort/quick/QuickSort2.java
@@ -0,0 +1,47 @@
+package algorithm.sort.quick;
+
+import util.ComUtil;
+
+public class QuickSort2 {
+    public static void main(String[] args) {
+        int[] a = {5,4,3,2,1};
+
+        quickSort(a,0,a.length-1);
+
+        ComUtil.print(a);
+    }
+
+    public static void quickSort(int[] a, int lo, int hi) {
+        if (lo >= hi) {
+            return;
+        }
+
+        int pivot = partition(a,lo,hi);
+
+        quickSort(a, lo, pivot - 1);
+        quickSort(a, pivot + 1, hi);
+    }
+
+    public static int partition(int[] a, int left, int right) {
+
+        int lo = left;
+        int hi = right;
+        int pivot = a[left];
+
+        while(lo < hi) {
+            while(lo < hi && a[hi] > pivot) {
+                hi--;
+            }
+
+            while(lo < hi && a[lo] <= pivot) {
+                lo++;
+            }
+
+            ComUtil.swap(a,lo,hi);
+        }
+
+        ComUtil.swap(a,lo,left);
+
+        return lo;
+    }
+}
